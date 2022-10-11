15bb36bb0f7c011939f42e34f5ed02e09f0edaeb
Rod Nolan <rnolan@aquent.com>
2022-10-11 12:05:27 -0400

    Day 6 AM Session 1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-// import logo from './logo.svg';
+import React from 'react';
+import { ComponentWithProps } from './ComponentWithProps';
+import asdbc from './logo.svg';
 // import './App.css';
 import { MyFirstComponent } from './MyFirstComponent';
 
 function App() {
   return <>
     <MyFirstComponent />
+    <SimpleComponent />
+    <SimpleList />
+    <SimpleListAsJSX />
+    <ComponentWithProps  attr2="second" attr1="first" attr3="third" />
+
+{/*
+
+  const props = {
+    attr2: "second",
+    attr1: "first"
+    attr3: "third"
+  };
+
+  return ComponentWithProps(props)
+
+*/}
+
+    <img src={asdbc} className="App-logo" alt="logo" height={1250} />
+    <img src={process.env.PUBLIC_URL + 'logo192.png'} alt="the company logo" height={250}/>
   </>
-
-
-  // return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload to see the changes,mzxbvklnbadslkj djklab dabjkhf aebdjhkf baeljhkfbej.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  // );
 }
 
 export default App;
+
+
+const SimpleComponent = () => React.createElement('div', null, "this is a plain old div tag");
+
+const SimpleList = () => React.createElement('ol', null, [
+  React.createElement('li', null, "item 1"),
+  React.createElement('li', null, "item 2"),
+  React.createElement('li', null, "item 3"),
+]);
+
+{/* <div>this is a plain old div tag</div> */}
+
+
+const SimpleListAsJSX = () => <ol>
+  <li>item 1</li>
+  <li>item 2</li>
+  <li>item 3</li>
+  <li>item 4</li>
+</ol>;
---
+++ src/ComponentWithProps.js
@@ -0,0 +1,3 @@
+export const ComponentWithProps = (props) => {
+    return <p>this is a custom component that accepts props: {props.attr1} {props.attr2} {props.attr3}</p>
+}

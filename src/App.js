b476434e45ada5f2d7ed866374cb3de9b0232a61
Rod Nolan <rnolan@aquent.com>
2022-10-10 21:35:23 -0400

    created and invoked a simple component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { MyFirstComponent } from './MyFirstComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <MyFirstComponent />
+  </>
+
+
+  // return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload to see the changes,mzxbvklnbadslkj djklab dabjkhf aebdjhkf baeljhkfbej.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  // );
 }
 
 export default App;
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,3 @@
+export function MyFirstComponent() {
+    return <p>Hello World!</p>
+}
